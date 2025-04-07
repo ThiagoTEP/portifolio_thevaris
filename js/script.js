20b1b4f8b7cf585b0ab755841fcf2253963ef16b
ThiagoTEP <thiagodepaulapt@gmail.com>
2025-04-07 12:00:51 +0100

    atualizacao de codigo @media

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+/* icon nav*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
 /* scrol sections active*/
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
@@ -20,4 +29,9 @@ window.onscroll = () => {
     let header = document.querySelector('header');
 
     header.classList.toggle('sticky', window.scroll > 100);
+
+    /* remove toggle icon and navbar when click navbar link  (scroll)*/
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 };
